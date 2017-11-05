a713097d49bb6ee1466d421defaa53b7793e785b
FranziskaMarieR <franziska.raufer@tum.de>
2017-11-05 01:00:16 +0100

    HelloUser is added!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		
+		Scanner read = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        HelloUser.userName = read.next();
+        read.close();
+        
+        HelloUser.greetUser();
 	}
-
 }
